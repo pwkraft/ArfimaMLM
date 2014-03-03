71cfbe8c45b472209351b3c66e81fe841566d6ba
pwkraft <kraft.pw@gmail.com>
2014-03-03 16:34:01 -0500

    minor bug fixes: fd for d>1, calculation of .xdif (3)

--- R/fd.numeric.R
+++ R/fd.numeric.R
@@ -1,8 +1,8 @@
 fd.numeric <-
 function(x, dval, ...) {
   if(dval!=0) {
-    if(dval>1)  x <- diffseries(x, 1)
-    else x <- diffseries(x, dval)
+    if (dval>1) dval <- 1
+    x <- diffseries(x, dval)
   }
   out <- list(series=x,estimator="external",value=dval)
   out
