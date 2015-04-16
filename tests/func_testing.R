6424a6cde5308d3b09f026eb79d08625c32924af
pwkraft <kraft.pw@gmail.com>
2015-04-16 19:29:39 -0400

    add github version test

--- tests/func_testing.R
+++ tests/func_testing.R
@@ -93,8 +93,36 @@ summary(m10)
 
 ### test github version
 
+rm(list=ls())
 library(devtools)
 install_github("pwkraft/ArfimaMLM")
 library(ArfimaMLM)
 
+# set basic parameters for simulation
+t = 100 # number of time points
+n = 500 # number of observations within time point
+N = t*n # total number of observations
 
+# generate fractional ARIMA Time Series for y_t, x1_t, z1_t, z2_t
+set.seed(123)
+y_t <- fracdiff.sim(t, d=0.4, mu=10)$series
+x1_t <- fracdiff.sim(t, d=0.3, mu=5)$series
+z1_t <- fracdiff.sim(t, d=0.1, mu=2)$series
+z2_t <- fracdiff.sim(t, d=0.25, mu=3)$series
+
+# simulate data
+data <- NULL; data$time <- rep(seq(1:t),each=n)
+data <- data.frame(data)
+data$x1 <- rnorm(N,rep(x1_t,each=n),2)
+data$x2 <- rnorm(N,0,40)
+data$z1 <- rnorm(N,rep(z1_t,each=n),3)
+data$z2 <- rep(z2_t,each=n)
+b1 <- 0.2+rep(rnorm(t,0,0.1),each=n)
+data$y <- (b1*data$x1-0.05*data$x2+0.3*rep(z1_t,each=n)
+           +0*data$z2+rnorm(N,rep(y_t,each=n),1))
+
+# estimate models
+m1 <- arfimaMLM(y.ydif ~ x1.xdif + x2 + z1.fd + z2.fd +(1|time), timevar="time"
+                ,data=data ,d="ML")
+m1b <- arfimaMLM(y.ydif ~ x1.xdif + x2 + z1.fd + z2.fd +(1|time), timevar="time"
+                 ,data=data ,d=0)
